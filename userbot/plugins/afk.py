2908842a9bd2cf1b48b4a196e87a84df4f908429
darklord9899 <47493597+darklord9899@users.noreply.github.com>
2020-05-26 23:37:26 +0530

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -24,7 +24,7 @@ async def set_not_afk(event):
         try:
             await borg.send_message(  # pylint:disable=E0602
                 Config.PLUGIN_CHANNEL,  # pylint:disable=E0602
-                "My Master has gone for some Important work he is very busy or he is lazy to see your messages🥳🥳🥳"
+                "Mine Owner has gone for some Important work he is very busy🥳🥳🥳"
             )
         except Exception as e:  # pylint:disable=C0103,W0703
             await borg.send_message(  # pylint:disable=E0602
@@ -61,9 +61,9 @@ async def _(event):
             afk_time = datetime.datetime.now()  # pylint:disable=E0602
         USER_AFK = f"yes: {reason}"  # pylint:disable=E0602
         if reason:
-            await event.edit(f"My Master is busy, and Reason is {reason}")
+            await event.edit(f"Mera Malik Ja Raha Hai, and Reason is {reason}")
         else:
-            await event.edit(f"My Master is currently Busy or Asleep ")
+            await event.edit(f"Mera Malik Ja Raha Hai")
         await asyncio.sleep(5)
         await event.delete()
         try:
@@ -124,7 +124,7 @@ async def on_afk(event):
         message_to_reply = f"My Master Has Been Gone For {afk_since}\nWhere He Is: Tereko kyu batau " + \
             f"\n\n__ I'll back in a few hours__\n**REASON**: {reason}" \
             if reason \
-            else f"**lol**\n\n
+            else f"**Important Notice**\n\n[This User Is Ded Forever...](https://telegra.ph//file/a53fa950ff31781d5930a.jpg) "
         msg = await event.reply(message_to_reply)
         await asyncio.sleep(5)
         if event.chat_id in last_afk_message:  # pylint:disable=E0602
